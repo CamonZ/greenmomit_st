f596402baaf538b69a1d7b00e70a5b34cbc618c3
Simon Garcia <simon.garciar@gmail.com>
2014-09-07 23:49:27 +0200

    make the request from the angular dashboards controller to fetch the thermostats the user has

--- packages/dashboard/public/controllers/dashboard.js
+++ packages/dashboard/public/controllers/dashboard.js
@@ -1,8 +1,17 @@
 'use strict';
 
-angular.module('mean.dashboard').controller('DashboardController', ['$scope', 'Global', 'Dashboard',
-  function($scope, Global, Dashboard) {
+angular.module('mean.dashboard').
+  controller('DashboardController', ['$http', '$scope', '$rootScope', 'Global', 'Dashboard',
+  function($http, $scope, $rootScope, Global, Dashboard) {
     $scope.global = Global;
+
+    var user = window.user;
+    console.log(user);
+    $http.get('/thermostats', { params: { sessionToken: user.sessionToken } }).
+      success(function(response){
+        console.log(response);
+      });
+
     $scope.package = {
       name: 'dashboard'
     };
