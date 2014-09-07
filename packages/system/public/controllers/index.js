8c262fd74e8692164c021b4144273a742c07b053
Simon Garcia <simon.garciar@gmail.com>
2014-09-07 22:30:22 +0200

    login into the greenmomit api and redirect the angular view to the dashboard

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -1,7 +1,14 @@
 'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
-  function($scope, Global) {
+angular.module('mean.system').controller('IndexController', ['$http', '$location', '$scope', '$rootScope', 'Global',
+  function($http, $location, $scope, $rootScope, Global) {
     $scope.global = Global;
+
+    $http.get('/greenmomit_login')
+      .success(function(response){
+        console.log(response);
+        $rootScope.user = response;
+        $location.url('/dashboard');
+      });
   }
 ]);
