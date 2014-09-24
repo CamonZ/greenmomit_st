cfa91f60b98e1715c2a54c0c94f00456dffe4167
Simon Garcia <simon.garciar@gmail.com>
2014-09-24 14:36:38 +0200

    return data from the db on the thermostats controller

--- packages/thermostats/server/controllers/thermostats_controller.js
+++ packages/thermostats/server/controllers/thermostats_controller.js
@@ -1,18 +1,34 @@
 'use strict';
 
-var greenMomitThermostats = require('../../../greenmomit_api/server/models/thermostat');
+var mongoose = require('mongoose'),
+    Thermostat = mongoose.model('Thermostat'),
+    ThermostatMeasurement = mongoose.model('ThermostatMeasurement');
 
 exports.index = function(req, res){
-  greenMomitThermostats.getThermostats(req.query.sessionToken, res);
+  Thermostat.find({}, function(err, docs){
+    res.json(docs);
+  });
 };
 
 exports.show = function(req, res){
-  greenMomitThermostats.getThermostatDetails(
-    req.query.sessionToken,
-    req.params.thermostatId,
-    res);
+  Thermostat.findOne({_id: req.params.thermostatId}, function(err, doc){
+    res.json(doc);
+  });
 };
 
 exports.historic = function(req, res){
-  res.json([]);
+  var query = ThermostatMeasurement.find({thermostatId: req.params.thermostatId}).limit(20); //return the last 20 measurements
+  query.exec(function(err, docs){
+    res.json(docs);
+  });
+  
 };
+
+function getDayDate(hour, min, sec, milli){
+  var d = new Date();
+  d.setHours(hour, min, sec, milli);
+  return d;
+}
+
+function startOfToday(){ return getDayDate(0, 0, 0, 0); }
+function endOfToday(){ return getDayDate(23, 59, 59, 999); }
